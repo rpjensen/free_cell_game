b6176cee5ac93c7a02f1398f4b4f07a8f83dffad
Joseph Tamberino <jtamber2@gmail.com>
2015-02-13 22:28:14 -0500

    Added a card prefab, sprites for the card, script for the card, script for the deck class. Methods in card includes checking card color, and deck script has shuffle and draw methods.

---
+++ Assets/_Scripts/Card.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Suit {
+	Club = 1,
+	Diamond,
+	Spade,
+	Heart
+}
+
+public enum CardColor {
+	Black,
+	Red
+}
+
+public class Card : MonoBehaviour {
+	[SerializeField]
+	Suit i_theSuit;
+	[SerializeField]
+	int i_value;
+	public Suit theSuit {
+		get {
+			return i_theSuit;
+		} set{
+			i_theSuit = value;}
+		}
+	public int value { 
+		get{
+			return i_value;
+				}
+		set{
+			i_value = value;	}
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//Method that checks if the card color is black
+	public CardColor whichColor(){
+		int suitInt = (int)this.theSuit;
+		if (suitInt % 2 != 0) return CardColor.Black;
+		return CardColor.Red;
+	}
+
+}
---
+++ Assets/_Scripts/Deck.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+	// set in Unity inspector
+	public List<Sprite> cardSprites;
+	public List<GameObject> cards;
+	public GameObject cardPrefab;
+	public bool ____________________________________;
+	//Set dynamically
+
+	// Use this for initialization
+	void Start () {
+		cards = new List<GameObject>();
+			int i = 0; 
+			int currentSuit = 0;
+			for (int j=1; j<5; j++){ //loop for each suit
+				currentSuit++;//adds 1 to the suit number
+				for (int k=1; k<14; k++){//for each card in the suit
+					GameObject tempCard = Instantiate (cardPrefab) as GameObject; //creates the card
+					tempCard.GetComponent<Card>().theSuit = (Suit)currentSuit; //sets suit to the current suit
+					tempCard.GetComponent<Card>().value = k; // sets value for the card
+					tempCard.GetComponentInChildren<SpriteRenderer>().sprite = cardSprites[i]; //sets sprite of the card
+				i++;
+				cards.Add(tempCard); //adds card to the deck
+				}
+			}
+		cards = shuffle (cards);
+
+
+				
+	}
+
+	//method to draw a card
+	public GameObject drawCard(){
+		GameObject tempCard = cards[cards.Count]; //gets card at the top of the deck
+		cards.RemoveAt (cards.Count); //removes the top card from the deck
+		return tempCard; //returns the card
+		}
+
+	//shuffle the deck
+	public List<GameObject> shuffle(List<GameObject> originalDeck){
+		List<GameObject> originList = originalDeck; //sets originList to the passed list
+		List<GameObject> tempCardList = new List<GameObject>(); //creates a second list
+		while (originList.Count > 0) { //runs as long as the original deck still has cards
+			GameObject tempCard = originList [Random.Range (0, originList.Count)]; //randomly chooses one card from the original list
+			tempCardList.Add (tempCard); //adds that card to the new list
+			originList.Remove (tempCard); //removes that card from the original list
+				}
+		return tempCardList; //returns the new shuffled list
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
