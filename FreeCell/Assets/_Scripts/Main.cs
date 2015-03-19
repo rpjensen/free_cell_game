4775a6fd4f53a6634cb91c7823af5df3a023921b
borisssss <basarkis@unca.edu>
2015-03-19 10:34:39 -0400

    Began Main Menu
    Started Main Menu

--- FreeCell/Assets/_Scripts/Main.cs
+++ FreeCell/Assets/_Scripts/Main.cs
@@ -367,6 +367,8 @@ public class Main : MonoBehaviour {
 
 	// Reload the level
 	void GameOver() {
+		// Invoke high scores here
+		// 
 		Application.LoadLevel ("_GameScene");
 	}
 
---
+++ FreeCell/Assets/_Scripts/Menu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button quitText;
+
+	void Start ()
+		
+	{
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		startText = startText.GetComponent<Button> ();
+		quitText = quitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+		
+	}
+	
+	public void ExitPress()
+		
+	{
+		quitMenu.enabled = true; 
+		startText.enabled = false; 
+		quitText.enabled = false;
+		
+	}
+	
+	public void NoPress()
+		
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		quitText.enabled = true;
+		
+	}
+	
+	public void StartLevel ()
+		
+	{
+		Application.LoadLevel ("_GameScene"); 
+	}
+	
+	public void ExitGame () 
+		
+	{
+		Application.Quit(); 
+		
+	}
+
+}
