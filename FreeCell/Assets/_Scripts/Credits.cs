4f2ce20ab6fa451fed786c22b23b2846a31ca3a1
Joseph Tamberino <jtamber2@gmail.com>
2015-03-22 22:05:49 -0400

    Actually added Credits scripts to github this time

---
+++ FreeCell/Assets/_Scripts/Credits.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public void BackToMenu(){
+		Application.LoadLevel ("_MainMenu");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
